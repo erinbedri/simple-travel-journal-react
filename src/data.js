a559cf22bf60715b5569334f99c37f6909888b25
erinbedri <erinbedri@gmail.com>
2023-01-09 13:32:29 +0100

    fixed image urls

--- src/data.js
+++ src/data.js
@@ -24,4 +24,56 @@ export default [
     imageUrl:
       "https://images.unsplash.com/photo-1652438171926-aa5fc0680451?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
   },
+  {
+    id: 3,
+    title: "Mount Everest",
+    location: "Nepal",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Mount+Everest/@27.9874063,86.9172314,17z/data=!3m1!4b1!4m5!3m4!1s0x39f8f9b9076e5c23:0x569f874b5f5ce5f5!8m2!3d27.9874063!4d86.9194201",
+    startDate: "12 Mar, 2021",
+    endDate: "16 Mar, 2021",
+    description:
+      "Mount Everest is the highest mountain in the world, with a peak elevation of 8,848 meters (29,029 feet). It is located in the Himalayas and straddles the border between Nepal and China.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1516616370751-86d6bd8b0651?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 4,
+    title: "The Great Wall of China",
+    location: "China",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/The+Great+Wall+of+China/@40.4319085,116.5703749,11z/data=!3m1!4b1!4m5!3m4!1s0x35f05e086a9b6a9d:0xa4c4b1a4a2c2f4b8!8m2!3d40.4319085!4d116.5703749",
+    startDate: "22 Mar, 2021",
+    endDate: "26 Mar, 2021",
+    description:
+      "The Great Wall of China is a series of fortifications that were built across the northern borders of ancient China to protect the Chinese states and empires against the nomadic groups of the Eurasian Steppe. It is the longest wall in the world, stretching over 21,196 kilometers (13,171 miles).",
+    imageUrl:
+      "https://images.unsplash.com/flagged/photo-1552553030-837c9c2b0fac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    id: 5,
+    title: "Niagara Falls",
+    location: "United States and Canada",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Niagara+Falls/@43.0781496,-79.0753547,11z/data=!3m1!4b1!4m5!3m4!1s0x89d2557a9f9f6cc7:0x646a9a7e6e33fbf1!8m2!3d43.0781496!4d-79.0753547",
+    startDate: "01 Apr, 2021",
+    endDate: "03 Apr, 2021",
+    description:
+      "Niagara Falls is a collective name for three waterfalls that straddle the international border between Canada and the United States. The three falls are the Horseshoe Falls, the American Falls, and the Bridal Veil Falls.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1612479338654-b0681c82d092?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 6,
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12ae401e824b9b:0x5017d681632bfc0!8m2!3d-33.8567844!4d151.2152967",
+    startDate: "10 Apr, 2021",
+    endDate: "14 Apr, 2021",
+    description:
+      "The Sydney Opera House is a performing arts center in Sydney, New South Wales, Australia. It is one of the most iconic buildings in the world and is a UNESCO World Heritage Site. The Opera House was designed by Danish architect Jørn Utzon and was completed in 1973.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1528072164453-f4e8ef0d475a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  },
 ];
