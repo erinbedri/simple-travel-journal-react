7a7b42e23c68809ea943e86c48e5ce1ffc41eea0
erinbedri <erinbedri@gmail.com>
2023-01-09 16:11:29 +0100

    implementer dark header mode

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   ));
   return (
     <>
-      <Header />
+      <Header darkMode={false} />
       <section className="experiences">{experiences}</section>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Header() {
+export default function Header(props) {
+  const [darkMode, setMode] = useState(props.darkMode);
+
+  function onClickHandler() {
+    setMode((darkMode) => !darkMode);
+  }
+
   return (
-    <header className="header">
+    <header className={`header ${darkMode ? "darkMode" : ""}`}>
       <img
         src={require("../img/logo.png")}
         alt="logo"
         className="header--logo"
       />
       <h1 className="header--title">my travel journal.</h1>
+      <button onClick={onClickHandler} className="mode-btn">
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
     </header>
   );
 }
